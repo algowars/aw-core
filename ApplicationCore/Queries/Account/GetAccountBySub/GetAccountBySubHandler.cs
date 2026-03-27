d4b019211bbfb6198496e36ffece99246953bb37
admclamb <adylanmclamb@yahoo.com>
2026-03-27 00:04:01 -0400

    Setting up getting account by sub

---
+++ ApplicationCore/Queries/Account/GetAccountBySub/GetAccountBySubHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApplicationCore.Dtos;
+using Ardalis.Result;
+
+namespace ApplicationCore.Queries.Account.GetAccountBySub;
+
+public sealed class GetAccountBySubHandler() : IQueryHandler<GetAccountBySubQuery, AccountDto>
+{
+    public async Task<Result<AccountDto>> Handle(
+        GetAccountBySubQuery request,
+        CancellationToken cancellationToken
+    ) { }
+}
---
+++ ApplicationCore/Queries/Account/GetAccountBySub/GetAccountBySubQuery.cs
@@ -0,0 +1,5 @@
+﻿using ApplicationCore.Dtos;
+
+namespace ApplicationCore.Queries.Account.GetAccountBySub;
+
+public sealed record GetAccountBySubQuery(string Sub) : IQuery<AccountDto>;
